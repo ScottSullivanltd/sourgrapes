5a3dc41f89f13c5138cb1ef94c2ebbeb86921b54
Scott Sullivan <scottsullivanltd@gmail.com>
2022-03-27 13:45:24 -0600

    Test: User story 6, feature winemaker ordered by most recent

--- app/models/winemaker.rb
+++ app/models/winemaker.rb
@@ -1,9 +1,17 @@
 class Winemaker < ApplicationRecord
   has_many :wines
-  
+
   validates_presence_of :name
   validates_presence_of :region
   validates :public_tasting, inclusion: [true, false]
   validates_presence_of :vineyard_acreage
+
+  def self.most_recent
+    order(created_at: :desc)
+  end
+
+  def formatted_date
+    self.created_at.strftime("%B %d %Y %I:%M %P %Z")
+  end
 
 end
--- spec/features/winemakers/index_spec.rb
+++ spec/features/winemakers/index_spec.rb
@@ -9,9 +9,19 @@ require 'rails_helper'
        visit "/winemakers"
 
        expect(page).to have_content(winemaker1.name)
-       # expect(page).to have_content("Region: #{winemaker1.region}")
        expect(page).to have_content(winemaker2.name)
-       # expect(page).to have_content("Region: #{winemaker2.region}")
+     end
+   end
+
+   describe 'User story 6', type: :feature do
+     it "can see winemakers ordered by most recent and when it was created" do
+       winemaker1 = Winemaker.create!(name: "Mondovete", region: "Napa Valley", public_tasting: false, vineyard_acreage: 85)
+       winemaker2 = Winemaker.create!(name: "Wet Creek", region: "Sonoma", public_tasting: true, vineyard_acreage: 120)
+
+       visit "/winemakers"
+
+       expect(page).to have_content("#{winemaker2.name} #{winemaker2.formatted_date}")
+       expect(page).to have_content("#{winemaker1.name} #{winemaker1.formatted_date}")
      end
    end
  end
