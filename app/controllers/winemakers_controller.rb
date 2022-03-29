8312e1750ed597aa7cb858957bd9ffb15dc3eb25
Scott Sullivan <scottsullivanltd@gmail.com>
2022-03-28 23:49:00 -0600

    Test: User story 11, Winemaker Index page link to form that creates new winemaker

--- app/controllers/winemakers_controller.rb
+++ app/controllers/winemakers_controller.rb
@@ -6,4 +6,23 @@ class WinemakersController < ApplicationController
   def show
     @winemaker = Winemaker.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    winemaker = Winemaker.create(winemaker_params)
+    redirect_to "/winemakers"
+  end
+
+  private
+  def winemaker_params
+    {
+      name: params[:name],
+      region: params[:region],
+      public_tasting: params[:public_tasting],
+      vineyard_acreage: params[:vineyard_acreage].to_i
+    }
+    params.permit(:name, :region, :public_tasting, :vineyard_acreage)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/winemakers', to: 'winemakers#index'
+  get '/winemakers/new', to: 'winemakers#new'
   get '/winemakers/:id', to: 'winemakers#show'
   get '/wines', to: 'wines#index'
   get '/wines/:id', to: 'wines#show'
   get '/winemakers/:winemaker_id/wines', to: 'winemaker_wines#index'
+  post '/winemakers', to: 'winemakers#create'
 end
