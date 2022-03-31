a54c1727a4120ee395f019aea827925b552064ab
Scott Sullivan <scottsullivanltd@gmail.com>
2022-03-30 22:48:13 -0600

    Test: User Story 22, User Story 23, delete from Winemaker Index and Wine Index pages

--- spec/features/wine/index_spec.rb
+++ spec/features/wine/index_spec.rb
@@ -110,4 +110,17 @@ require 'rails_helper'
        expect(page).to have_content(false)
      end
    end
+
+   describe 'User Story 23, Wine Delete From Wine Index Page', type: :feature do
+     it "Wine Index page has link to delete a Wine then return to Wine Index page" do
+       winemaker1 = Winemaker.create!(name: "Mondovete", region: "Napa Valley", public_tasting: false, vineyard_acreage: 85)
+       wine1 = winemaker1.wines.create!(name: "Red Vine", blend: "Cabernet", vintage: "2002", barrels_produced: 25, signature_label: false)
+
+       visit "/wines"
+       click_link "Delete Wine"
+
+       expect(current_path).to eq("/wines")
+       expect(page).to_not have_content(wine1)
+     end
+   end
  end
